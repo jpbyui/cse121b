c9927adf70bdf99edabbfa05a675d2934a526618
jpbyui <juan.lds@gmail.com>
2024-01-21 01:39:40 -0300

    001

---
+++ scripts/w02-task.js
@@ -0,0 +1,31 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Juan Pablo Rodriguez Pedrozo';
+let currentYear = new Date().getFullYear;
+let profilePicture = '/images/IMG-20201023.jpg'
+
+/* Step 3 - Element Variables */
+
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.createElement('img');
+imageElement.src = profilePicture;
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
